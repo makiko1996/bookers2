120c2a86e1529d4439f8dc7eb008f1905280ed2f
Makikotakemori <t_makiko_1996@yahoo.co.jp>
2021-11-22 13:52:23 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+     redirect_to book_path(@book), notice: 'You have created book successfully .'
+    else
+      @books.all
+     render :index
+    end
   end
   
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_132215) do
+ActiveRecord::Schema.define(version: 2021_11_22_083234) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "user_image_id"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
